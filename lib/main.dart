05b3dda19e671580dbe36fa044c17b15b4b3a95e
Sameh Nagi <eng.same7.la@gmail.com>
2023-08-20 21:46:52 +0200

    Adding calculator logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Myapp());
+void main() => runApp(MyApp());
 
-class Myapp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +22,7 @@ class _CalculatorState extends State<Calculator> {
     return Container(
       child: ElevatedButton(
         onPressed: () {
-          // TODO add function for button press
+          calculation(btnTxt);
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: btnColor,
@@ -54,17 +54,17 @@ class _CalculatorState extends State<Calculator> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             // Calculator display.
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Padding(
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(10.0),
                   child: Text(
-                    '0',
+                    text,
                     textAlign: TextAlign.left,
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Colors.white,
-                      fontSize: 100,
+                      fontSize: 50,
                     ),
                   ),
                 )
@@ -154,5 +154,135 @@ class _CalculatorState extends State<Calculator> {
         ),
       ),
     );
+  }
+
+  // Calculator logic
+  dynamic text = '0';
+  double firstNum = 0;
+  double secondNum = 0;
+
+  dynamic result = '';
+  dynamic finalResult = '';
+  dynamic opr = '';
+  dynamic preOpr = '';
+  void calculation(btnText) {
+    if (btnText == 'AC') {
+      // Resets all variables to their initial values, effectively resetting the calculator.
+      resetCalculator();
+    } else if (opr == '=' && btnText == '=') {
+      // Perform the calculation based on the previous operator (preOpr).
+      performOperation();
+    } else if (btnText == '+' ||
+        btnText == '-' ||
+        btnText == 'x' ||
+        btnText == '/' ||
+        btnText == '=') {
+      // Handles the arithmetic operations by updating the firstNum, secondNum,
+      // opr, and preOpr variables accordingly and performing the corresponding
+      // calculation.
+      if (firstNum == 0) {
+        firstNum = double.parse(result);
+      } else {
+        secondNum = double.parse(result);
+      }
+
+      if (opr == '+') {
+        finalResult = add();
+      } else if (opr == '-') {
+        finalResult = sub();
+      } else if (opr == 'x') {
+        finalResult = mul();
+      } else if (opr == '/') {
+        finalResult = div();
+      }
+      preOpr = opr;
+      opr = btnText;
+      result = '';
+    } else if (btnText == '%') {
+      // Calculates the percentage of numOne and assigns the result to result
+      // after checking for decimal places.
+      result = firstNum / 100;
+      finalResult = doesContainDecimal(result);
+    } else if (btnText == '.') {
+      if (!result.toString().contains('.')) {
+        //result = result.toString() + '.';
+        result = '${result.toString()}.';
+      }
+      finalResult = result;
+    } else if (btnText == '+/-') {
+      result.toString().startsWith('-')
+          ? result = result.toString().substring(1)
+          : result = '-${result.toString()}';
+      finalResult = result;
+    } else {
+      // If the input is any other digit or operator, it appends the input
+      // to the result.
+      result = result + btnText;
+      finalResult = result;
+    }
+
+    // In Flutter, the setState() method is used to notify the framework that
+    // the internal state of a widget has changed and that the UI should be
+    // rebuilt to reflect those changes. It's commonly used in the context of
+    // stateful widgets.
+    // Update the calculator's displayed text with the final result.
+    setState(() {
+      text = finalResult;
+    });
+  }
+
+  void resetCalculator() {
+    text = '0';
+    firstNum = 0;
+    secondNum = 0;
+    result = '';
+    finalResult = '0';
+    opr = '';
+    preOpr = '';
+  }
+
+  void performOperation() {
+    if (preOpr == '+') {
+      finalResult = add();
+    } else if (preOpr == '-') {
+      finalResult = sub();
+    } else if (preOpr == 'x') {
+      finalResult = mul();
+    } else if (preOpr == '/') {
+      finalResult = div();
+    }
+  }
+
+  String add() {
+    result = (firstNum + secondNum).toString();
+    firstNum = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String sub() {
+    result = (firstNum - secondNum).toString();
+    firstNum = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String mul() {
+    result = (firstNum * secondNum).toString();
+    firstNum = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String div() {
+    result = (firstNum / secondNum).toString();
+    firstNum = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String doesContainDecimal(dynamic result) {
+    if (result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if (!(int.parse(splitDecimal[1]) > 0))
+        return result = splitDecimal[0].toString();
+    }
+    return result;
   }
 }
